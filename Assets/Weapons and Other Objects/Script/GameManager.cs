4843b0aad645f0a082d34ba7fb5a6effd20dd0f0
Eric Gilmore <epg5@njit.edu>
2017-01-22 14:26:02 -0500

    fixed procedural logic bug and added mannequins

--- Assets/Weapons and Other Objects/Script/GameManager.cs
+++ Assets/Weapons and Other Objects/Script/GameManager.cs
@@ -11,7 +11,6 @@ public class GameManager : MonoBehaviour {
 
     public Dungeon dungeon;
 
-
     public Player player1;
     public Player player2;
 
---
+++ Assets/Weapons and Other Objects/Script/MannequinChallenge.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MannequinChallenge : MonoBehaviour {
+
+    GameObject[] mannequins;
+
+	// Use this for initialization
+	void Start () {
+        mannequins = GameObject.FindGameObjectsWithTag("mannequin");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (checkForMannequins())
+        { 
+            //game over you win
+            Application.Quit();
+        }
+	}
+
+    bool checkForMannequins()
+    { 
+        bool found = false;
+        for (int i = 0; i < mannequins.Length; i++)
+        {
+            if (mannequins[i].activeSelf == true)
+            {
+                found = true;
+            }
+        }
+        return found;
+    }
+}
